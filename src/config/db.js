094fa3498aab6dfabab9f26eec913d709f4bdaf1
Pratip Maity <pratipmaity500@gmail.com>
2025-05-29 02:27:24 +0530

    bot responce change

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://pratipmaity500:Pratip%40123@chatbot.yihi59o.mongodb.net/?retryWrites=true&w=majority&appName=chatbot');
+        // const conn = await mongoose.connect('mongodb+srv://pratipmaity500:Pratip%40123@chatbot.yihi59o.mongodb.net/?retryWrites=true&w=majority&appName=chatbot');
+        const conn = await mongoose.connect('mongodb://localhost:27017/chatbot');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
